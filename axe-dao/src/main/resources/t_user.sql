f98d43303e2a50b0818b3259599b0e36d4be8e07
zhaohua.li <zhaohua.li@tqmall.com>
2015-11-09 01:06:22 +0800

    修改date source到映射文件

---
+++ axe-dao/src/main/resources/t_user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `t_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) NOT NULL,
+  `user_age` varchar(20) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8
