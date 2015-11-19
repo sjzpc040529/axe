42a0a94a2b6924c100f0421dc787c0858b00c1d6
zhaohua.li <zhaohua.li@tqmall.com>
2015-11-19 10:55:49 +0800

    job的三种模式

--- axe-study/src/main/java/org/lzh/framework/axe/study/quartz/Job1.java
+++ axe-study/src/main/java/org/lzh/framework/axe/study/quartz/Job1.java
@@ -12,10 +12,26 @@ import java.util.Date;
  * @version: V1.0
  */
 public class Job1 implements Serializable {
-    public void doJob(){
+    public void doJob() throws InterruptedException {
         System.out.println("不继承QuartzJobBean方式-调度进行中..."+
                 StringUtils.dateFormat(new Date())+
                 "==>name:"+Thread.currentThread().getName()+
                 " ==>threadId:"+Thread.currentThread().getId());
+        Thread.sleep(1000);
+//        if(Integer.parseInt(Thread.currentThread().getName().split("-")[1])%2==0){
+//            data2(Thread.currentThread().getName());
+//        }else {
+//            data1(Thread.currentThread().getName());
+//        }
+
+    }
+
+    private String data1(String name){
+        System.out.println(name+"单数");
+        return null ;
+    }
+
+    private void data2(String name){
+        System.out.println(name+"双数");
     }
 }
